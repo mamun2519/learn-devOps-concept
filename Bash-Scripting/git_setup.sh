46787a6edeee084b4d1db44980cd6f433d298b69
mamun2519 <juborajislam46@gmail.com>
2024-11-24 22:42:09 +0600

    add to convention

--- Bash-Scripting/git_setup.sh
+++ Bash-Scripting/git_setup.sh
@@ -1,5 +1,5 @@
 1. git access
 2. local development env
     1. code clone => script ("shell/bash", python, js)
-    2. systeconverntion => scriptm requirements check => (shell script)
+    2. system requirements check => (shell script)
     3. roles
