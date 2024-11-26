2670d1dff5a6cc6adfffbd99e2fb7a476cf2d161
mamun2519 <juborajislam46@gmail.com>
2024-11-26 20:23:56 +0600

    added to condifion

--- Bash-Scripting/variable.sh
+++ Bash-Scripting/variable.sh
@@ -1,6 +1,12 @@
 
 ENV_FILE=".env"
 user_name=$TEST_USER
-
+if [-f $ENV_FILE]; then
+		source $ENV_FILE
+		echo "$ENV_File loadded"
+else
+		echo "$ENV_FILE not  found"
+		exist 1
+fi
 echo "A new user created: $user_name"
 echo "$TEST_USER"
